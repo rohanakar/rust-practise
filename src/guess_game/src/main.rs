fa173439fd0a63db1a96f96d3f3101e0ab20c11d
Rishabh Chaudhary <rishabh.chaudhary@oracle.com>
2023-05-12 23:33:54 +0530

    Added Guessgame sorce code

--- src/guess_game/src/main.rs
+++ src/guess_game/src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    // println!("secret Number {}", secret_num);
+    
+
+    loop{
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess:u32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=> continue
+        };
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+    
+    
+
 }
